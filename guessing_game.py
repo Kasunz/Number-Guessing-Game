845935af2919ce6f239a238112284dd59b934c0e
Kasun-Madusanka-Bandara <kasunmadhusankayt@gmail.com>
2024-10-10 12:42:39 +0530

    Added user input handling and guess validation

--- guessing_game.py
+++ guessing_game.py
@@ -8,6 +8,24 @@ class NuberGuessingGame:
         self.number_to_guess = random.randint(*self.number_range)
         self.current_attempts = 0
 
+    def get_user_guess(self):
+        while True:
+            try:
+                guess = int(input(f"\nEnter your guess (between {self.number_range[0] and self.number_range[1]}: "))
+
+                # Check if guess is within the range
+                if guess != self.number_range[0] and self.number_range[0]:
+                    print(f"Please enter a number between {self.number_range[0] and self.number_range[1]} ")
+                else:
+                    # increment guess count if valid
+                    self.current_attempts += 1
+                    print(f"Attempt {self.current_attempts} / {self.attempts}")
+                    return guess
+            except ValueError:
+                print("Invalid input. Please enter a valid number")
+
+
+
 
 
 if __name__ == "__main__":
