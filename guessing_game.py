16319cf62f4a34d29332c617cf7ce1dd0327b593
Kasun-Madusanka-Bandara <kasunmadhusankayt@gmail.com>
2024-10-10 13:21:54 +0530

    Implemented user input, guess checking, and game loop

--- guessing_game.py
+++ guessing_game.py
@@ -12,20 +12,41 @@ class NuberGuessingGame:
         while True:
             try:
                 guess = int(input(f"\nEnter your guess (between {self.number_range[0] and self.number_range[1]}: "))
-
-                # Check if guess is within the range
-                if guess != self.number_range[0] and self.number_range[0]:
-                    print(f"Please enter a number between {self.number_range[0] and self.number_range[1]} ")
-                else:
-                    # increment guess count if valid
-                    self.current_attempts += 1
-                    print(f"Attempt {self.current_attempts} / {self.attempts}")
-                    return guess
+                return guess
             except ValueError:
                 print("Invalid input. Please enter a valid number")
 
+    # Check if the guess is correct, too high, or too low.
+    def check_guess(self, guess):
+        if guess == self.number_to_guess:
+            print(f'congratulations! you guess the correct number {self.number_to_guess} in {self.current_attempts} attempts!')
+            return True
+        elif guess > self.number_to_guess:
+            print("Your guess is too high!")
+        else:
+            print("your guess is too low!")
+        return False
+
+    # Run the main loop and print outputs
+    def play_game(self, guess):
+        print("Welcome to the Number Guessing Game!")
 
 
+        while self.current_attempts < self.attempts:
+            guess = self.get_user_guess()
+
+            if self.check_guess(guess):
+                break
+
+            if self.current_attempts == self.attempts:
+                print(f"Sorry, you've used all {self.attempts} attempts. The correct number was {self.number_to_guess}.")
+
+
+    def ask_play_again(self):
+        pass
+
+    def reset_game(self):
+        pass
 
 
 if __name__ == "__main__":
